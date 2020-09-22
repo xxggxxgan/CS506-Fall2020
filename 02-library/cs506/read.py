ad7f9ba1ddb8108932508398f14b56dfd42c856c
xiaoxin Gan <xiaoxingan@gxx.local>
2020-09-21 22:01:34 -0400

    lab2

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,11 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open (csv_file_path, "r") as file:
+    line = file.readlines()
+    for l in line:
+        l = l.split(",")
+        for i in range(len(l)):
+            l[i] = eval(l[i])
+        result.append(l)
+    return result
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,36 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
-
+    if (len(x) != len(y)):
+        raise ValueError("Error! lengths must be equal")
+    result = sum([abs(x[i] - y[i])**2 for i in range(len(x))])**(1/2)
+    return result
+    
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    if (len(x) != len(y)):
+        raise ValueError("Error! lengths must be equal")
+    result = sum([abs(x[i] - y[i]) for i in range(len(x))])
+    return result
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if (len(x) != len(y)):
+        raise ValueError("Error! lengths must be equal")
+    result = 0
+    if (x == []):
+        result = 0
+    elif (y == []):
+        result = 0
+    else:
+        result = 1 - ((len(set(x).intersection(set(y))))/len(set(x).union(set(y))))
+    return result
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if (len(x) != len(y)):
+        raise ValueError("Error! lengths must be equal")
+    x_len = sum([num**2 for num in x])**0.5
+    y_len = sum([num**2 for num in y])**0.5
+    if x_len == 0 or y_len == 0:
+        raise ValueError("Error! lengths must not be zero")
+    inner_product = sum([x[i] * y[i] for i in range(len(x))])
+    result = inner_product / (x_len * y_len)
+    return result
 
 # Feel free to add more
